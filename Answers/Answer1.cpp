c82c1d32ac7cfe0fb5173f47d8dea0479428626d
Prajjwal Dubey <92323404+PrajjwalDy@users.noreply.github.com>
2022-10-01 15:30:15 +0530

    Answer of Question 1
    This code contain only logic of the problem

---
+++ Answers/Answer1.cpp
@@ -0,0 +1,33 @@
+int search(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        int s=0;
+        int e=n-1;
+        
+        int mid=s+(e-s)/2;
+        
+        while(s<=e)
+        {
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            
+            else if(nums[mid]>target)
+            {
+                e=mid-1;
+            }
+            
+            else if(nums[mid]<target)
+            {
+                s=mid+1;
+            }
+            
+            
+            
+            mid=s+(e-s)/2;
+        }
+        
+        return -1;
+        
+    }
